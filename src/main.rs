129244281367022f8b476543dec5fc095b1217ce
Leonardo C. <leonardorudolfocardoso@gmail.com>
2026-06-03 15:59:56 -0300

    feat: read_request

--- src/main.rs
+++ src/main.rs
@@ -71,10 +71,43 @@ fn parse<'a>(raw: &'a RawRequest) -> Request<'a> {
     }
 }
 
-fn read<R: BufRead>(r: &mut R) -> RawRequest {
-    let mut buf = Vec::new();
-    r.read_to_end(&mut buf).unwrap();
-    buf
+fn read_request<R: BufRead>(reader: &mut R, buf: &mut RawRequest) {
+    loop {
+        let mut line = Vec::new();
+
+        let n = reader.read_until(b'\n', &mut line).unwrap();
+
+        if n == 0 {
+            break;
+        }
+
+        let end = line == b"\r\n";
+
+        buf.extend(line);
+
+        if end {
+            break;
+        }
+    }
+
+    let headers = std::str::from_utf8(buf).unwrap();
+
+    let content_length = headers
+        .lines()
+        .find_map(|line| {
+            let (key, value) = line.split_once(": ")?;
+            if key.eq_ignore_ascii_case("content-length") {
+                value.parse::<usize>().ok()
+            } else {
+                None
+            }
+        })
+        .unwrap_or(0);
+
+    let start = buf.len();
+
+    buf.resize(start + content_length, 0);
+    reader.read_exact(&mut buf[start..]).unwrap();
 }
 
 fn main() {
@@ -84,8 +117,9 @@ fn main() {
     loop {
         match listener.accept() {
             Ok((mut stream, _)) => {
-                let mut buf = BufReader::new(&stream);
-                let raw = read(&mut buf);
+                let mut reader = BufReader::new(&stream);
+                let mut raw = RawRequest::new();
+                read_request(&mut reader, &mut raw);
                 let request = parse(&raw);
                 let response = handle(&request);
 
