d33f4de59ec578e2e76a029cc03fe4af8cb47709
Leonardo C. <leonardorudolfocardoso@gmail.com>
2026-06-05 18:48:18 -0300

    feat: treat http errors

--- src/http.rs
+++ src/http.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, io::BufRead};
+use std::{collections::HashMap, fmt::Display, io::BufRead, str::Utf8Error};
 
 pub type RawRequest = Vec<u8>;
 pub type Headers<'a> = HashMap<&'a str, &'a str>;
@@ -29,7 +29,12 @@ impl<'a> Request<'a> {
 }
 
 #[derive(Debug)]
-pub struct InvalidRequest;
+pub enum InvalidRequest {
+    Malformed,
+    MalformedHeader,
+    Utf8Error(Utf8Error),
+}
+
 impl<'a> TryFrom<&'a RawRequest> for Request<'a> {
     type Error = InvalidRequest;
 
@@ -37,28 +42,30 @@ impl<'a> TryFrom<&'a RawRequest> for Request<'a> {
         let request_line_end = raw
             .windows(2)
             .position(|bytes| bytes == b"\r\n")
-            .ok_or(InvalidRequest)?;
+            .ok_or(InvalidRequest::Malformed)?;
         let request_line_bytes = &raw[..request_line_end];
-        let request_line = str::from_utf8(request_line_bytes).unwrap();
+        let request_line = str::from_utf8(request_line_bytes).map_err(InvalidRequest::Utf8Error)?;
         let mut parts = request_line.split_whitespace();
-        let method = parts.next().unwrap();
-        let path = parts.next().unwrap();
-        let version = parts.next().unwrap();
+        let method = parts.next().ok_or(InvalidRequest::Malformed)?;
+        let path = parts.next().ok_or(InvalidRequest::Malformed)?;
+        let version = parts.next().ok_or(InvalidRequest::Malformed)?;
 
         let header_start = request_line_end + 2;
         let header_end = header_start
             + raw[header_start..]
                 .windows(4)
                 .position(|bytes| bytes == b"\r\n\r\n")
-                .unwrap();
+                .ok_or(InvalidRequest::Malformed)?;
 
         let header_bytes = &raw[header_start..header_end];
-        let headers_text = str::from_utf8(header_bytes).unwrap();
+        let headers_text = str::from_utf8(header_bytes).map_err(InvalidRequest::Utf8Error)?;
 
         let mut headers = Headers::new();
 
         for line in headers_text.split("\r\n") {
-            let (key, value) = line.split_once(": ").unwrap();
+            let (key, value) = line
+                .split_once(": ")
+                .ok_or(InvalidRequest::MalformedHeader)?;
             headers.insert(key, value);
         }
 
@@ -71,6 +78,20 @@ impl<'a> TryFrom<&'a RawRequest> for Request<'a> {
     }
 }
 
+pub enum ReadError {
+    IO(std::io::Error),
+    Utf8(Utf8Error),
+}
+
+impl Display for ReadError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ReadError::IO(e) => write!(f, "ReadError: IO error: {e}"),
+            ReadError::Utf8(e) => write!(f, "ReadError: Utf8 error: {e}"),
+        }
+    }
+}
+
 pub enum ReadStatus {
     Complete,
     Closed,
@@ -80,11 +101,23 @@ pub struct Reader<R: BufRead> {
     inner: R,
 }
 
+impl From<Utf8Error> for ReadError {
+    fn from(value: Utf8Error) -> Self {
+        Self::Utf8(value)
+    }
+}
+
+impl From<std::io::Error> for ReadError {
+    fn from(value: std::io::Error) -> Self {
+        Self::IO(value)
+    }
+}
+
 impl<R: BufRead> Reader<R> {
     pub fn new(inner: R) -> Self {
         Self { inner }
     }
-    pub fn read(&mut self, buf: &mut RawRequest) -> std::io::Result<ReadStatus> {
+    pub fn read(&mut self, buf: &mut RawRequest) -> Result<ReadStatus, ReadError> {
         loop {
             let mut line = Vec::new();
 
@@ -103,7 +136,7 @@ impl<R: BufRead> Reader<R> {
             }
         }
 
-        let headers = std::str::from_utf8(buf).unwrap();
+        let headers = std::str::from_utf8(buf)?;
 
         let content_length = headers
             .lines()
